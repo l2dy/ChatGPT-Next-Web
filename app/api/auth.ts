50b6a29361a8e7bde473f4da6180c508b057052d
Zero King <l2dy@icloud.com>
2023-07-15 00:48:05 +0800

    Remove open rewrite endpoints

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -39,7 +39,7 @@ export function auth(req: NextRequest) {
   console.log("[User IP] ", getIP(req));
   console.log("[Time] ", new Date().toLocaleString());
 
-  if (serverConfig.needCode && !serverConfig.codes.has(hashedCode) && !token) {
+  if (serverConfig.needCode && !serverConfig.codes.has(hashedCode)) {
     return {
       error: true,
       msg: !accessCode ? "empty access code" : "wrong access code",
--- next.config.mjs
+++ next.config.mjs
@@ -58,20 +58,7 @@ if (mode !== "export") {
   };
 
   nextConfig.rewrites = async () => {
-    const ret = [
-      {
-        source: "/api/proxy/:path*",
-        destination: "https://api.openai.com/:path*",
-      },
-      {
-        source: "/google-fonts/:path*",
-        destination: "https://fonts.googleapis.com/:path*",
-      },
-      {
-        source: "/sharegpt",
-        destination: "https://sharegpt.com/api/conversations",
-      },
-    ];
+    const ret = [];
 
     const apiUrl = process.env.API_URL;
     if (apiUrl) {
